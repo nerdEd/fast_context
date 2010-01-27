3292f22ed74daa07da89e0c8932aa3276d17b0e6
Ed Schmalzle <Jonas714@gmail.com>
2010-01-26 19:58:25 -0500

    Added basic test

---
+++ test/fast_context_test.rb
@@ -0,0 +1,61 @@
+require 'rubygems'
+require 'test/unit'
+require 'redgreen'
+require 'shoulda'
+require 'active_support'
+require File.join(File.dirname( __FILE__), '..', 'lib', 'fast_context.rb')
+
+$TOP_SETUP_COUNT = 0
+$INNER_SETUP_COUNT = 0
+$INNER_MOST_SETUP_COUNT = 0
+
+class Fastfast_contextTest < ActiveSupport::TestCase
+  
+  fast_context "Top fast_context" do
+    setup do 
+      $TOP_SETUP_COUNT += 1
+    end
+    
+    should "test something" do
+      assert true
+    end
+    
+    should "and test something else" do
+      assert true
+    end
+    
+    fast_context "Inner fast_context" do
+      setup do
+        $INNER_SETUP_COUNT += 1
+      end
+      
+      should "test something in the inner fast_context" do
+        assert true
+      end
+      
+      should "and test something else in the inner fast_context" do
+        assert true
+      end
+      
+      fast_context "Inner-most fast_context" do
+        setup do 
+          $INNER_MOST_SETUP_COUNT += 1
+        end
+        
+        should "test something in the inner-most fast_context" do
+          assert true
+        end
+        
+        should "and test something else in teh inner-most fast_context" do
+          assert true
+        end        
+      end
+    end
+  end
+  
+  def test_setup_call_counts
+    assert_equal(3, $TOP_SETUP_COUNT)
+    assert_equal(2, $INNER_SETUP_COUNT)
+    assert_equal(1, $INNER_MOST_SETUP_COUNT)
+  end
+end
